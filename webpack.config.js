029191ea864d5fe07941806c1424025fac8a2097
Francisco Amorim <francisco.s.amorim@gmail.com>
2020-08-07 00:10:37 +0100

    Tutorial Part 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,68 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = [
-
-  // Webpack Renderer Configuration
-  {
-    mode: 'development',
-    entry: './src/index.ts',
-    target: 'electron-main',
-    resolve: {
-      alias: {
-        ['@']: path.resolve(__dirname, 'src')
-      },
-      extensions: ['.tsx', '.ts', '.js'],
+const electronConfiguration = {
+  mode: 'development',
+  entry: './src/index.ts',
+  target: 'electron-main',
+  resolve: {
+    alias: {
+      ['@']: path.resolve(__dirname, 'src')
     },
-    module: {
-      rules: [{
-        test: /\.ts$/,
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      include: /src/,
+      use: [{ loader: 'ts-loader' }]
+    }]
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js'
+  }
+};
+
+const reactConfiguration =  {
+  mode: 'development',
+  entry: './src/renderer.tsx',
+  target: 'electron-renderer',
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      ['@']: path.resolve(__dirname, 'src')
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
         include: /src/,
         use: [{ loader: 'ts-loader' }]
-      }]
-    },
-    output: {
-      path: __dirname + '/dist',
-      filename: 'index.js'
-    }
-  },
-
-  // Webpack Renderer Configuration
-  {
-    mode: 'development',
-    entry: './src/renderer.tsx',
-    target: 'electron-renderer',
-    devtool: 'source-map',
-    resolve: {
-      alias: {
-        ['@']: path.resolve(__dirname, 'src')
       },
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts(x?)$/,
-          include: /src/,
-          use: [{ loader: 'ts-loader' }]
-        },
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-            'sass-loader',
-          ],
-        }
-      ]
-    },
-    output: {
-      path: __dirname + '/dist',
-      filename: 'renderer.js'
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html'
-      })
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      }
     ]
-  }
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'renderer.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
+module.exports = [
+  electronConfiguration,
+  reactConfiguration
 ];
